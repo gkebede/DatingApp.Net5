3355533f45966c1de30bb5cb2bbd5c7b25146334
ghailu <ghailu1979@hotmail.com>
2020-09-27 02:26:26 -0400

    end of section 2

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using DatingApp_API.Models;
+using DatingAppAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+
+        //private readonly DbContext _dbContext;
+
+        public UsersController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUsers>>> GetUsers() 
+        {
+            return await  _dbContext.AppUsers.ToListAsync();
+         }
+
+        [HttpGet("{id}")]
+        public async Task <ActionResult<AppUsers>> GetUser(int id)
+        {
+            return  await _dbContext.AppUsers.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+    }
+}
---
+++ Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAppAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp_API.Models
+{
+    public class DataContext : DbContext 
+
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+                    : base(options)
+        {
+        }
+      public DbSet<AppUsers> AppUsers  { get; set; }
+    }
+}
